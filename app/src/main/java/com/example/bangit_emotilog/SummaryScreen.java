b9e8b060ddc47dbfaaf99dec98654fc281b49363
pbangit <bangit@ualberta.ca>
2025-09-29 13:13:06 -0600

    Added total count for individual emoji

--- app/src/main/java/com/example/bangit_emotilog/SummaryScreen.java
+++ app/src/main/java/com/example/bangit_emotilog/SummaryScreen.java
@@ -1,4 +1,6 @@
 package com.example.bangit_emotilog;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -7,13 +9,16 @@ import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 public class SummaryScreen extends AppCompatActivity {
     ListView listViewSummary;
-
     Button backToMain;
 
     @Override
@@ -25,26 +30,45 @@ public class SummaryScreen extends AppCompatActivity {
         backToMain = findViewById(R.id.backToMain);
 
         List<EmoticonLog> logs = LogRepository.getLogs();
+        SimpleDateFormat yearMonthDay = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        String today = yearMonthDay.format(new Date());
 
-        Map<String, Integer> counts = new LinkedHashMap<>();
+        Map<String, Integer> todayCounts = new LinkedHashMap<>();
+        int totalAllTime = logs.size();
+        for (EmoticonLog log : logs) {
+            String logDate = yearMonthDay.format(new Date(log.getTimeStamp()));
+            if (logDate.equals(today) ) {
+                String emoji = log.getEmoji();
+                todayCounts.put(emoji, todayCounts.getOrDefault(emoji, 0) + 1);
+            }
+        }
+
+
+        List<String> summaryItems = new ArrayList<>();
+        summaryItems.add("Today's count: ");
+        for (Map.Entry<String, Integer> entry : todayCounts.entrySet()) {
+            summaryItems.add(entry.getKey() + " logged today --> " + entry.getValue());
+        }
+
+        summaryItems.add("Total logs (all time) --> " + totalAllTime);
+
+        Map<String, Integer> totalCounts = new LinkedHashMap<>();
         for(EmoticonLog log : logs) {
             String emoji = log.getEmoji();
-            if (!counts.containsKey(emoji)) {
-                counts.put(emoji, 0);
-            }
-            counts.put(emoji,counts.get(emoji) + 1);
+            totalCounts.put(emoji, totalCounts.getOrDefault(emoji, 0) + 1);
         }
-        String[] summaryItems = new String[counts.size() + 1];
-        int i = 0;
-        int total = 0;
-        for(Map.Entry<String, Integer> entry : counts.entrySet()){
-            summaryItems[i++] = entry.getKey() + "-->" + entry.getValue();
-            total += entry.getValue();
+        summaryItems.add("");
+        summaryItems.add("All time counts per emoji: ");
+        for(Map.Entry<String, Integer> entry : totalCounts.entrySet()){
+            summaryItems.add(entry.getKey() + "total --> " + entry.getValue());
         }
-        summaryItems[i] = "Total --> " + total;
+
+
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                this, android.R.layout.simple_list_item_1,summaryItems
+                this,
+                android.R.layout.simple_list_item_1,
+                summaryItems
         );
         listViewSummary.setAdapter(adapter);
 
@@ -53,8 +77,6 @@ public class SummaryScreen extends AppCompatActivity {
             public void onClick(View v){
                 finish();
             }
-
         });
     }
-
 }
